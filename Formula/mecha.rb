28d4ad265c8ac3dbaef788dfeffca0f1845d9a80
xiaolai <lixiaolai@gmail.com>
2026-03-09 20:32:22 +0800

    fix: correct SHA256 hashes for v0.2.1

--- Formula/mecha.rb
+++ Formula/mecha.rb
@@ -7,20 +7,20 @@ class Mecha < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/xiaolai/mecha.im/releases/download/v#{version}/mecha-darwin-arm64.tar.gz"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      sha256 "017d85d960e0286dcce965eea37dbbdddc2d6a93939810d08090c2b75eb64e6e"
     else
       url "https://github.com/xiaolai/mecha.im/releases/download/v#{version}/mecha-darwin-x64.tar.gz"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      sha256 "e3557fe1cc616c4669bd00efe1ff86f94b48919aed9eb6f1132be3ea401a31e4"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/xiaolai/mecha.im/releases/download/v#{version}/mecha-linux-arm64.tar.gz"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      sha256 "168b35c321f61b98c528ad22bd40acd0b9eb2acc2a1676d63c6967d8af19c4de"
     else
       url "https://github.com/xiaolai/mecha.im/releases/download/v#{version}/mecha-linux-x64.tar.gz"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      sha256 "03805bfc9ede73d174c09d28fdb3b28866ee33b6e27a7264bba8e457080971aa"
     end
   end
 
