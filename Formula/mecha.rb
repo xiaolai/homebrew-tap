bb5218cde456d5818ae04a0ffb47f3988b30aa59
xiaolai <lixiaolai@gmail.com>
2026-03-09 16:25:35 +0800

    Add mecha formula

---
+++ Formula/mecha.rb
@@ -0,0 +1,36 @@
+class Mecha < Formula
+  desc "Run an army of Claude Code bots on your machines"
+  homepage "https://mecha.im"
+  version "0.2.0"
+  license "ISC"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/xiaolai/mecha.im/releases/download/v#{version}/mecha-darwin-arm64.tar.gz"
+      sha256 "PLACEHOLDER"
+    else
+      url "https://github.com/xiaolai/mecha.im/releases/download/v#{version}/mecha-darwin-x64.tar.gz"
+      sha256 "PLACEHOLDER"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/xiaolai/mecha.im/releases/download/v#{version}/mecha-linux-arm64.tar.gz"
+      sha256 "PLACEHOLDER"
+    else
+      url "https://github.com/xiaolai/mecha.im/releases/download/v#{version}/mecha-linux-x64.tar.gz"
+      sha256 "PLACEHOLDER"
+    end
+  end
+
+  def install
+    bin.install "mecha"
+    # SPA dashboard must live alongside the binary (mecha looks for <execPath>/spa/)
+    (bin/"spa").install Dir["spa/*"] if (buildpath/"spa").exist?
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/mecha --version")
+  end
+end
